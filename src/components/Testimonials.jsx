4ac9ce539dc77f3beb3bcb6c067a52070b43faa8
abedin-ansari <jainulansari579@gmail.com>
2025-08-11 10:15:57 +0530

    code updated

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -16,7 +16,7 @@ const Testimonials = () => {
       <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
         Customer
         <span className="underline underline-offset-4 decoration-1 font-light">
-          Testimonials
+          Testimonial
         </span>
       </h1>
       <p className="text-center text-gray-500 mb-12 max-w-80 mx-auto">
