cdea60792e25ff3b69e1af9546dc3d1df7271818
abedin-ansari <jainulansari579@gmail.com>
2025-08-11 12:34:47 +0530

    contact update

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,3 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-// import tailwindcss from "@tailwindcss/vite";
-// import path from "path";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//       "@utils": path.resolve(__dirname, "./utils"),
-//       "@assets": path.resolve(__dirname, "./src/assets"),
-//     },
-//   },
-//   build: {
-//     target: "esnext",
-//     modulePreload: false,
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
