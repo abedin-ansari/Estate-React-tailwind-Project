e4e5bb5a2ce1786352d182f680bf4a9efcc92b9c
abedin-ansari <jainulansari579@gmail.com>
2025-08-11 12:17:40 +0530

    all errors fixed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import tailwindcss from "@tailwindcss/vite";
+// import path from "path";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//       "@utils": path.resolve(__dirname, "./utils"),
+//       "@assets": path.resolve(__dirname, "./src/assets"),
+//     },
+//   },
+//   build: {
+//     target: "esnext",
+//     modulePreload: false,
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
